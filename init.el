703a77368aaf00310219a64fe6fbb6f70eae3921
Munyoung Hwang <munyoung.hwang@navercorp.com>
2020-09-11 16:58:56 +0900

    init: add kotlin support with lsp

--- init.el
+++ init.el
@@ -153,7 +153,7 @@
        (javascript       ; all(hope(abandon(ye(who(enter(here))))))
         +lsp)
        ;;julia             ; a better, faster MATLAB
-       ;; kotlin            ; a better, slicker Java(Script)
+       (kotlin +lsp)            ; a better, slicker Java(Script)
        latex             ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
